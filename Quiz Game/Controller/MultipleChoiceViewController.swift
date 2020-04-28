6e6759787a6665cf93628fce428c44a82b2890f3
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-04-28 05:09:33 -0400

    Initialise Multiple Choice

--- Quiz Game/Controller/MultipleChoiceViewController.swift
+++ Quiz Game/Controller/MultipleChoiceViewController.swift
@@ -10,13 +10,48 @@ import UIKit
 
 class MultipleChoiceViewController: UIViewController {
 
+    
+    private let contentView =  UIView()
+    private var contentViewConstraints: [NSLayoutConstraint]!
+    
+    private let questionView = UIView()
+    private var questionViewConstraints: [NSLayoutConstraint]!
+    
+    private let answerView = UIView()
+    private var answerViewConstraints: [NSLayoutConstraint]!
+    
+    private let countDownView = UIView()
+    private var countDownViewConstraints: [NSLayoutConstraint]!
+    
+    private let questionLabel = RoundedLabel()
+    private var questionLabelConstraints: [NSLayoutConstraint]!
+    private let questionButton = RoundedButton()
+    private var questionButtonConstraints: [NSLayoutConstraint]!
+    
+    private var answerButtons = [RoundedButton]()
+    private var answerButtonsConstraints: [NSLayoutConstraint]!
+    
+    private let progressView = UIProgressView()
+    private var progressViewConstraints: [NSLayoutConstraint]!
+    
+    private let backgroundColour = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
+    private let foregroundColour = UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = UIColor.white
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.tintColor = .systemBlue
+        view.backgroundColor = backgroundColour
+        
+        layoutView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = false
+        
+    }
+    
+    func layoutView() {
+        
     }
 }
